c2d11da7b2ef3d534baa4720724f17bec5a9e4d5
Alvaro Escobar <alvaroescobaronline@gmail.com>
2019-09-13 14:00:30 -0400

    Eager loading with eager_load (application:108.9ms ActiveRecord: 56.7ms)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.order(created_at: :desc).eager_load(:author)
+    @posts = Post.order(created_at: :desc).eager_load(:author).where("authors.name = ?", "John")
   end
 
   # GET /posts/1
