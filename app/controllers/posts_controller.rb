fd0c170fd71f6318a2ea38960f9366b18b9ef72a
Alvaro Escobar <alvaroescobaronline@gmail.com>
2019-09-13 14:07:17 -0400

    Eager loading with includes (application:1707.9ms ActiveRecord: 49.5ms)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.order(created_at: :desc).eager_load(:author).where("authors.name = ?", "John")
+    @posts = Post.order(created_at: :desc).includes(:author)
   end
 
   # GET /posts/1
