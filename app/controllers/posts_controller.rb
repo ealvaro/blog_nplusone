5ef85832bcb00d19e92db794949e5d0274258eda
Alvaro Escobar <alvaroescobaronline@gmail.com>
2019-09-13 13:47:56 -0400

    Eager loading with preload (application:1731.0ms ActiveRecord:46.8ms)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.order(created_at: :desc).preload(:author)
+    @posts = Post.order(created_at: :desc).preload(:author)      #.where("authors.name = ?", "John") does not work
   end
 
   # GET /posts/1
